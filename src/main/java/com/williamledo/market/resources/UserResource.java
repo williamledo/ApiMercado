9e7569555b6306289c77429543047c4f1a26e9ba
William Lêdo <williamledoc@gmail.com>
2023-02-15 15:26:33 -0300

    User insert

--- src/main/java/com/williamledo/market/resources/UserResource.java
+++ src/main/java/com/williamledo/market/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.williamledo.market.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -7,8 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.williamledo.market.entities.User;
 import com.williamledo.market.services.UserService;
@@ -34,6 +39,13 @@ public class UserResource {
 		User obj  = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 		
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert (@RequestBody User obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 	
 }
