d027a15f0e439bddc8d63c9b962ee532188fc07b
William Lêdo <williamledoc@gmail.com>
2023-02-15 16:34:44 -0300

    User update

--- src/main/java/com/williamledo/market/services/UserService.java
+++ src/main/java/com/williamledo/market/services/UserService.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.williamledo.market.entities.User;
 import com.williamledo.market.repositories.UserRepository;
@@ -34,6 +36,20 @@ public class UserService {
 	
 	public void delete(Long id) {
 		repository.deleteById(id);
+	}
+
+	public User update(Long id, User obj) {
+		User entity = repository.getReferenceById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+	
+	private void updateData(User entity, User obj) {
+
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+		
 	}
 	
 }
